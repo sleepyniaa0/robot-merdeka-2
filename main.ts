3d4fda22875a4969a6cb21db04182230c662b14e
sleepyniaa0 <142799829+sleepyniaa0@users.noreply.github.com>
2023-08-21 19:34:34 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        servos.P1.setAngle(180)
+        basic.pause(500)
+        servos.P2.setAngle(180)
+        basic.pause(500)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.InBackground)
+    } else {
+        servos.P1.setAngle(0)
+        servos.P2.setAngle(0)
+    }
+})
