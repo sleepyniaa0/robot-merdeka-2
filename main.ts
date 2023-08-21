c31530612b1d5ecd2ee9302ec25f1dbabf8a7d3a
sleepyniaa0 <142799829+sleepyniaa0@users.noreply.github.com>
2023-08-21 20:38:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ basic.forever(function () {
         basic.pause(500)
         servos.P2.setAngle(180)
         basic.pause(500)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.InBackground)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wedding), music.PlaybackMode.InBackground)
     } else {
         servos.P1.setAngle(0)
         servos.P2.setAngle(0)
